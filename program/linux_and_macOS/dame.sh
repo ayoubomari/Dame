4490e04c612060bc68d48290e71d2ae2df9bb96d
OMARI Ayoub <ayoubomari799@gmail.com>
2022-04-28 17:12:18 +0000

    relative path

--- program/linux_and_macOS/dame.sh
+++ program/linux_and_macOS/dame.sh
@@ -1,1 +1,11 @@
-java -jar dame.jar
+#!/bin/sh
+
+# Retrieve dame directory
+PROGRAM=`readlink "$0"`
+if [ "$PROGRAM" = "" ]; then
+  PROGRAM=$0
+fi
+PROGRAM_DIR=`dirname "$PROGRAM"`
+
+# Run Dame
+exec java -jar "$PROGRAM_DIR"/dame.jar
